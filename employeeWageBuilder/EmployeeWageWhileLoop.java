1763c78c321f054821c6fcd81ddcb2bd084de948
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-07-19 13:15:17 +0530

    Added : employee wage using while loop

---
+++ employeeWageBuilder/EmployeeWageWhileLoop.java
@@ -0,0 +1,41 @@
+package employeeWageBuilder;
+
+public class EmployeeWageWhileLoop {
+	public static void main(String[] args) {
+		final int isFullTime = 1; 
+		final int isPartTime = 2;
+		final int isAbsent = 3;
+		final int maxWorkingHrs = 100;
+		final int maxWorkingDays = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		int empRatePerHr = 20;
+		int totalEmpHrs = 0;
+		int days = 0;
+	
+		while(days < maxWorkingDays && totalEmpHrs <= maxWorkingHrs) {
+			int empCheck = (int)(Math.random()*3)+1;
+			days++;
+			switch(empCheck) {
+			case isFullTime :
+				empHrs = 8;	 
+				System.out.println("Employee is full time");
+				break;
+			case isPartTime :
+				empHrs = 4;
+				System.out.println("Employee is part time");
+			    break;
+			case isAbsent :    
+				System.out.println("Employee is absent");
+				empHrs = 0;
+				break;
+			}
+			totalEmpHrs += empHrs;
+		}	
+			System.out.println("Total employee hours : "+totalEmpHrs);
+			empWage = totalEmpHrs*empRatePerHr;
+			System.out.println("employee Monthly wage : "+empWage);
+		}
+
+}
