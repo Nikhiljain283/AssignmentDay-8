5a5b7ea24f1cb1105e714b5b7abfcb120892f644
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-07-19 13:07:44 +0530

    Added : employee part time wage

---
+++ employeeWageBuilder/EmployeePartTime.java
@@ -0,0 +1,29 @@
+package employeeWageBuilder;
+
+public class EmployeePartTime {
+	public static void main(String[] args) {
+		final int isFullTime = 1; 
+		final int isPartTime = 2;
+		final int isAbsent = 3;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		int empRatePerHr = 20;
+			int empCheck = (int)(Math.random()*3)+1;
+	
+			if(empCheck == isFullTime) {
+				System.out.println("Employee is full time");
+				empHrs = 8;
+			}else if(empCheck == isPartTime){
+				System.out.println("Employee is part time");
+				empHrs = 4;
+			}else {
+				System.out.println("Employee is absent");
+				empHrs = 0;
+			}
+			empWage = empHrs*empRatePerHr;
+			System.out.println("employee daily wage : "+empWage);
+		}
+		
+
+}
