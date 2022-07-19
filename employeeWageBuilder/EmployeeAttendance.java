cd6107acb512fa6c38d47bbffcb804258e623978
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-07-19 13:03:21 +0530

    Added : employee attendance

---
+++ employeeWageBuilder/EmployeeAttendance.java
@@ -0,0 +1,19 @@
+package employeeWageBuilder;
+
+public class EmployeeAttendance {
+	public static void main(String[] args) {
+		final int present = 1; 
+		final int absent = 2;
+			int empCheck = (int)(Math.random()*2)+1;
+	
+			if(empCheck == present) {
+				System.out.println("Employee is present");
+			}else
+				System.out.println("Employee is absent");
+
+			}	
+		}
+		
+	
+
+
