4bb0cd276e12d1b9765ab0022e623ac82df6935d
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-07-19 13:09:48 +0530

    Added : employee wage by switch method

---
+++ employeeWageBuilder/EmployeeWageSwitch.java
@@ -0,0 +1,33 @@
+package employeeWageBuilder;
+
+public class EmployeeWageSwitch {
+		public static void main(String[] args) {
+			final int isFullTime = 1; 
+			final int isPartTime = 2;
+			final int isAbsent = 3;
+			
+			int empHrs = 0;
+			int empWage = 0;
+			int empRatePerHr = 20;
+				int empCheck = (int)(Math.random()*3)+1;
+		
+				switch(empCheck) {
+				case isFullTime :
+					System.out.println("Employee is full time");
+					empHrs = 8;
+					break;
+				case isPartTime :
+					System.out.println("Employee is part time");
+					empHrs = 4;
+				    break;
+				case isAbsent :    
+					System.out.println("Employee is absent");
+					empHrs = 0;
+					break;
+				}
+				empWage = empHrs*empRatePerHr;
+				System.out.println("employee daily wage : "+empWage);
+			}
+			
+
+}
